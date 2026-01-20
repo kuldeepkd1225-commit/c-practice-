e81f42e7ea43a9f898adc7f92b5add04ef8f5a47
kuldeepkd1225-commit <kuldeep.kd1225@gmail.com>
2026-01-20 06:54:28 +0530

    Program to search an array.

---
+++ array_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+    // Code here
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+           return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int x;
+    printf("Enter the element to search: ");
+    scanf("%d", &x);
+    int result = search(arr, n, x);
+    if(result != -1) {
+        printf("Element found at index: %d\n", result);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+    return 0;
+}
